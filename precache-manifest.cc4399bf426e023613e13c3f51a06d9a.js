18582d1108880c7bd1a75a965d44226260d01283
arun0808rana <arun0808rana@gmail.com>
2020-07-30 10:55:09 +0530

    Updates

---
+++ precache-manifest.cc4399bf426e023613e13c3f51a06d9a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59eaf461ad4bd32a2adfb51fcb07aedb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f5c1b42deade028058d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d8fe2afe8fd8295285fc",
+    "url": "/static/css/main.632259e4.chunk.css"
+  },
+  {
+    "revision": "3f5c1b42deade028058d",
+    "url": "/static/js/2.b211ac34.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b211ac34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8fe2afe8fd8295285fc",
+    "url": "/static/js/main.a5a399b5.chunk.js"
+  },
+  {
+    "revision": "71bda13dac4c7864234f",
+    "url": "/static/js/runtime-main.27162009.js"
+  },
+  {
+    "revision": "9322ec6fd714d8e1265c2a3e87797391",
+    "url": "/static/media/sterioRecord.9322ec6f.png"
+  }
+]);
